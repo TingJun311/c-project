5cfd26c58c5997a8f3952d71c3f68444743ff761
Isaac <chewtingjun311@gmail.com>
2022-09-16 12:47:52 +0800

    ft_split

---
+++ ft_split/ft_split.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+unsigned int malloc_count(char *str)
+{
+	unsigned int c;
+
+	c = 0;
+	while (*str)
+	{
+		if (*str == 32 || (*str >= 9 && *str <= 13))
+			c++;
+		while (*str && (*str == 32 || (*str >= 9 && *str <= 12)))
+			str++;
+		str++;
+	}
+	if (*str == '\0')
+		c++;
+	return (c);
+}
+
+unsigned int count_word(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] && (str[i] != 32 || !(str[i] >= 9 && str[i] <= 32)))
+		i++;
+	return (i);
+}
+
+char	**ft_split(char *str)
+{
+	char	**matrix;
+	unsigned int oc;
+	unsigned int ic;
+	unsigned int c;
+
+	while (*str && (*str == 32 || (*str >= 9 && *str <= 13)))
+		str++;
+	c = malloc_count(str);
+	matrix = (char **)malloc(sizeof(char *) * (c + 1));
+	oc = 0;
+	while (oc < c)
+	{
+		ic = count_word(str);
+		matrix[oc] = (char *)malloc(sizeof(char) * (ic + 1));
+		ic = 0;
+		while (*str)
+		{
+			if (*str == 32 || (*str >= 9 && *str <= 13))
+				break;
+			matrix[oc][ic] = *str;
+			ic++;
+			str++;
+		}
+		matrix[oc][ic] = '\0';
+		while (*str && (*str == 32 || (*str >= 9 && *str <= 13)))
+			str++;
+		oc++;
+	}
+	return (matrix);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, &*str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+
+}
+
+int	main(int ac, char **av)
+{
+	char	**str;
+	int	o;
+
+	if (ac == 2)
+	{
+		o = 0;
+		str = ft_split(av[1]);
+		while (str[o])
+		{
+			ft_putstr(str[o]);
+			o++;
+		}
+		free(str);
+
+	}
+	write(1, "\n", 1);	
+	return (0);
+}
